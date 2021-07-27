47ca2c86e7d5cc2f07628b593d868a1c19390f3b
Ben Armstead <benarmstead@protonmail.com>
2021-07-27 08:14:09 +0000

    Comment code

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ use std::path::Path;
 use walkdir::WalkDir;
 
 fn main() -> std::io::Result<()> {
+    // Get arguments from CLI
     let args: Vec<String> = env::args().collect();
+    
     let pwd: String;
-
+    
+    // If the length of the passed arguments is less than 2 (non passed).
+    // Then, cat_r in the current directory.
+    // If arguments are passed, cat_r the first one
     if args.len() < 2 {
         let path = env::current_dir()?;
         pwd = path.display().to_string();
@@ -15,9 +20,9 @@ fn main() -> std::io::Result<()> {
         pwd = args[1].to_string();
     }
 
+    // For each file in the directory, if its a file (not directory), then print its contents
     for entry in WalkDir::new(&pwd) {
         let path = entry?.path().display().to_string();
-
         let is_it_file = Path::new(&path).is_file();
         
         if is_it_file == true{
