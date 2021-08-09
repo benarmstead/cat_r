77484276180965a00921958a971a19f2ce8118e5
Ben Armstead <benarmstead@protonmail.com>
2021-08-09 22:48:45 +0100

    Lint code

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() -> std::io::Result<()> {
         let path = entry?.path().display().to_string();
         let is_it_file = Path::new(&path).is_file();
         
-        if is_it_file == true{
+        if is_it_file{
             let contents = fs::read_to_string(&path).expect("stream did not contain valid UTF-8");
             println!("{}", contents);
         }
